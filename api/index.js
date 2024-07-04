4755a48d573e08af199fdeed3a6cd35c565c0d9e
Sudharsan <sudhars4012@gmail.com>
2024-07-04 07:42:03 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
-  //to do imports like this we have to declare type:module in package json
+//to do imports like this we have to declare type:module in package json
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to Mongodb");
+  })
+  .catch((error) => console.log(error));
 const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
